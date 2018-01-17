61654824fe3b49eb31a740edf2f4575249a84d73
Natalie Melo <natalie.melo@yale.edu>
2018-01-17 19:58:17 +0000

    adding checks to written hws in ap

--- cs50/2017/ap/house/check50/__init__.py
+++ cs50/2017/ap/house/check50/__init__.py
@@ -1,8 +1,15 @@
 from check50 import *
-
+import os
 
 class House(Checks):
 
     @check()
     def submitted(self):
         """ "Around the House" submitted"""
+        if not any(os.path.exists("house.{}".format(extension))
+                    for extension in ["doc", "docx", "pdf", "txt"]):
+            raise Error("File not found")
+
+#    @check("submitted")
+#    def wordcount(self):
+#
